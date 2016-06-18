5a7f39876f27bf363ba8241186c62a6660e6e31d
Huy <jacobhtnguyen@gmail.com>
2016-06-18 15:37:18 -0400

    created basic classes

--- main.cpp
+++ main.cpp
@@ -1,26 +1,58 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
 using namespace std;
 
-
-class Node
+enum Entity : int
 {
-public:
-	int index;   // index of node
-	string institution;  // institution type of node
-	int edges; // edges of the node
-	double rating; // rate of the node
-
+	dealer = 1,
+	investor = 2,
+	issuer = 3
 };
 
 class Edge
 {
 public:
-	int from; // index of start edge
-	int to; // index of end edge
+	short int from; // index of start edge
+	short int to; // index of end edge
+	short int comm_chan; // communication channel
 };
 
+class Node
+{
+public:
+	short int id;   // id of node
+	string type;  // institution type of node
+	short int edges; // edges of the node
+	double rating; // rate of the node
+
+};
+
+class NodeSet
+{
+public:
+	double cost;
+	Node node;
+
+};
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+	char data;
+	int count;
+	ifstream nodes_world ("nodes_world.txt"), edges_world ("edges_world.clq");
+	ofstream result;
+	string line;
+
+	if (edges_world.is_open())
+	{
+		getline (edges_world, line);
+		while ( getline (edges_world, line) )
+		{
+			cout << line << '\n';
+		}
+	}
+	else cout << "Unable to open file, incorrect directory" << endl;
+
+	return 0;
 }
